0f6a727d90408d998c137ad054e5a199e2fca392
JohnGoni <wkdrjs4365@gmail.com>
2022-08-25 10:50:22 +0900

    8.25 오전

--- 12.개인작업/static/img/marker.py
+++ 12.개인작업/static/img/marker.py
@@ -9,33 +9,34 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 
-df = pd.read_csv('./광주_자동차정비업체.csv')
-map = folium.Map(location=[df.위도.mean(), df.경도.mean()], zoom_start=17,
+df2 = pd.read_csv('./광주_자동차정비업체.csv')
+df1 = pd.read_csv('./사고지점.csv')
+map = folium.Map(location=[df2.위도.mean(), df2.경도.mean()], zoom_start=17,
 max_bounds=True,
 min_zoom=17,
 max_zoom=17,
-min_lat=35.1615,
-max_lat=35.1615,
-min_lon=126.9042,
-max_lon=126.9042,
+min_lat=df1.위도[0],
+max_lat=df1.위도[0],
+min_lon=df1.경도[0],
+max_lon=df1.경도[0],
 zoom_control=False,
 no_touch=True)
-for i in df.index:
+for i in df2.index:
     folium.Marker(
-        location=[df.위도[i], df.경도[i]],
-        popup=folium.Popup(f"<a href='https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query={df.장소[i]}', target=_'blink'>{df.도로명주소[i]}</a>",
+        location=[df2.위도[i], df2.경도[i]],
+        popup=folium.Popup(f"<a href='https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query={df2.장소[i]}', target=_'blink'>{df2.도로명주소[i]}</a>",
         max_width=100),
-        tooltip=df.장소[i]
+        tooltip=df2.장소[i]
     ).add_to(map)
 folium.Marker(
-    location=[35.161894610275986, 126.90426748208054],
-    popup=folium.Popup('광주광역시 북구 신안동 162-1', max_width=200),
+    location=[df1.위도[0], df1.경도[0]],
+    popup=folium.Popup(df1.도로명주소[0], max_width=200),
     tooltip='사고위치',
     icon=folium.Icon('darkred', icon='ok')
 ).add_to(map)
 folium.CircleMarker(
     radius=200,
-    location=[35.161894610275986, 126.90426748208054],
+    location=[df1.위도[0], df1.경도[0]],
     color="#ffffff",        # RGB, 16진수
     fill=True,
     fill_color="ffffff"
